aaa81778f7e1e30bbff0dcbe0785c4c84052cf2e
Hazrul <hi@hazrulazhar.com>
2021-02-05 18:11:56 +0800

    updated the menus

---
+++ autoload/RestaurantController.php
@@ -0,0 +1,28 @@
+<?php
+// Class that provides methods for working with the form data.
+// There should be NOTHING in this file except this class definition.
+
+class SimpleController {
+	private $mapper;
+
+	public function __construct() {
+		global $f3;						// needed for $f3->get()
+		$this->mapper = new DB\SQL\Mapper($f3->get('DB'),"restaurants");	// create DB query mapper object
+																			// for the "simpleModel" table
+    }
+    
+    public function putIntoDatabase($data) {
+		$this->mapper->save();									// save new record with these fields
+	}
+
+	public function getData() {
+		$list = $this->mapper->find();
+		return $list;
+	}
+
+	public function deleteFromDatabase($idToDelete) {
+		$this->mapper->load(['id=?', $idToDelete]);				// load DB record matching the given ID
+		$this->mapper->erase();									// delete the DB record
+	}
+
+}
