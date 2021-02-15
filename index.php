6cd366c731828c112e5cc0e045c16998cd7d11e8
Hazrul <hi@hazrulazhar.com>
2021-02-15 14:21:18 +0800

    fixed some errors in front controller

--- index.php
+++ index.php
@@ -46,14 +46,13 @@ $f3->route('GET /restaurants',
 // Show the Add restaurants page.
 $f3->route('GET /restaurants/add',
   function ($f3) {
-    $controller = new RestaurantController;
-    $data = $controller->getRestaurant();
     $f3->set('html_title','Restaurant - Whats4Lunch - The World\'s easiest Food Delivery for people with diets and allergies');
     $f3->set('content','restaurants/add.html');
     echo Template::instance()->render('layout.html');
   }
 );
 
+// Todo: modify the comment to add the id value in getRestaurant
 // Show the Show restaurants Page
 $f3->route('GET /restaurants/show',
   function ($f3) {
