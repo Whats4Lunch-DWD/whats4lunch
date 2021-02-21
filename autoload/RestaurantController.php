7aeee5437fc6e916308e975306e4b9f53ea8936f
Hazrul <hi@hazrulazhar.com>
2021-02-21 17:44:53 +0800

    removed the print options

--- autoload/RestaurantController.php
+++ autoload/RestaurantController.php
@@ -27,7 +27,6 @@ class RestaurantController {
 	}
 
 	public function findRestaurants($options) {
-		print_r($options);
 		if ($options["choicestyle"]=="3choices") {
 			$list = $this->mapper->find();
 		} else {
