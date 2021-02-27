d1fb318e3a13a5729ba86208a2e7cb44c752c539
Hazrul <hi@hazrulazhar.com>
2021-02-27 16:56:51 +0800

    test resto data

--- index.php
+++ index.php
@@ -109,7 +109,7 @@ $f3->route('GET /restaurants/show/@id',
     //print_r($args);
     $data = $controller->getRestaurant($args['id']);
     $f3->set('result',$data);
-    print_r($data);
+    //print_r($data);
     $f3->set('html_title','Restaurant - Whats4Lunch - The World\'s easiest Food Delivery for people with diets and allergies');
     $f3->set('content','restaurants/show.html');
     echo Template::instance()->render('layout.html');
