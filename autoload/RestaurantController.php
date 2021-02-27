03d9eb44fe4fa3d4dfa08ce2cfd071098b1ad192
Hazrul <hi@hazrulazhar.com>
2021-02-27 17:15:40 +0800

    added a cart

--- autoload/RestaurantController.php
+++ autoload/RestaurantController.php
@@ -8,6 +8,8 @@ class RestaurantController {
 
 	public function __construct() {
 		global $f3;
+		global $cart;
+
 		$this->mapper = new DB\SQL\Mapper($f3->get('DB'),"restaurants");	// create DB query mapper object
 																			// for the "restaurants" table
 		$this->menus_mapper = new DB\SQL\Mapper($f3->get('DB'),"menus");
