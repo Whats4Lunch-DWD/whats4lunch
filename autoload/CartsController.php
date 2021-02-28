6da48fc16dd7edfd455ab611ceeb0c013a77f1a3
Hazrul <hi@hazrulazhar.com>
2021-02-28 14:43:23 +0800

    test add to cart

--- autoload/CartsController.php
+++ autoload/CartsController.php
@@ -15,7 +15,7 @@ class CartsController {
     }
     
     public function add($id){
-        $menu_item = $this->menus_mapper->load(['id=?', $id]);
+        $menu_item = $this->menus_mapper->find(['id=?', $id]);
 
         print_r($menu_item);
         
