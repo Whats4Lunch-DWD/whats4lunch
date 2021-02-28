b07eb8b4cb81327697da6c5a229e08ceaaa0b860
Hazrul <hi@hazrulazhar.com>
2021-02-28 19:27:06 +0800

    test add to cart

--- autoload/CartsController.php
+++ autoload/CartsController.php
@@ -41,16 +41,19 @@ class CartsController {
         //echo "menu_id from cart_items: ".$cart_items[0]["menu_id"]."<br />";
         //echo "id from add function: ".$id."<br />";
 
-        foreach ($menu_item as $item_key => $item_value) {
-            //echo $item_key."=>".$item_value."<br />";
-            if ($item_key != "created_at" ) {
-                if ($item_key == "id") {
-                    $this->cart_items_mapper["menu_id"]=$item_value;
-                } else {
-                    $this->cart_items_mapper[$item_key]=$item_value;
+        if ($cart_items[0]["menu_id"] != $id) {
+            foreach ($menu_item as $item_key => $item_value) {
+                echo $item_key."=>".$item_value."<br />";
+                if ($item_key != "created_at" ) {
+                    if ($item_key == "id") {
+                        $this->cart_items_mapper["menu_id"]=$item_value;
+                    } else {
+                        $this->cart_items_mapper[$item_key]=$item_value;
+                    }
                 }
-            }
+            }   
         }
+
 
         if ($cart_items[0]["id"]>0) {
             $this->cart_items_mapper["id"]=$cart_items[0]["id"];
