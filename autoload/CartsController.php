3df8c8d446a3f257b1f94a58294b82b74a70cf1c
Hazrul <hi@hazrulazhar.com>
2021-02-28 19:59:19 +0800

    test add to cart

--- autoload/CartsController.php
+++ autoload/CartsController.php
@@ -62,7 +62,7 @@ class CartsController {
         $this->cart_items_mapper["cart_id"] = $cart_session[0]["id"];
         $this->cart_items_mapper["quantity"]=$cart_items["quantity"]+1;
 
-                
+        /*
         echo "<pre>";
         print_r($this->cart_items_mapper["cart_id"]);
         echo "<br />";
@@ -74,7 +74,7 @@ class CartsController {
         print_r($this->cart_items_mapper);
         echo "</pre>";
         die();
-        
+        */
         
         $this->cart_items_mapper->save();
     }
